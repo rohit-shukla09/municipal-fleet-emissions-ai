1e15086c3e5fb70ab92687eb9d77cabe60f3b635
rohit-shukla09 <shuklarohit819@gmail.com>
2026-09-20 19:51:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -31,10 +31,10 @@ st.set_page_config(
 hide_header_style = """
 <style>
     [data-testid="stHeader"] {visibility: hidden;}
+    footer {visibility: hidden;}
 </style>
 """
 st.markdown(hide_header_style, unsafe_allow_html=True)
-# -------------------------------
 
 
 def _version_tuple(v: str):
